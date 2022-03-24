ac58eceec34a6ae6dfb78a3c89ba2f6d01e6cc74
Ashour100 <interinstitute5@gmail.com>
2022-03-24 14:40:58 +0100

    update

--- main.js
+++ main.js
@@ -7,15 +7,14 @@ const app= new Vue({
         
     },
     mounted(){
-        generateEmails=function(){
             const self=this;
             for(let i=0;i<10;i++){
                 axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
                 .then (function(result){
-                    self.emails[i]=result.response.response;
+                    self.emails[i]=result.data.response;
                     console.log(i);
                 });
             }
-        }
+            console.log(self.emails)
     }
 });
