e7c67899c9e907f4aa23466528ba334e85464db3
Ashour100 <interinstitute5@gmail.com>
2022-03-24 14:16:11 +0100

    Fondamentals added

--- main.js
+++ main.js
@@ -1,8 +1,21 @@
 const app= new Vue({
     el:'#app',
     data:{
-        
+        emails:[]
     },
     methods:{
+        
+    },
+    mounted(){
+        generateEmails=function(){
+            const self=this;
+            for(let i=0;i<10;i++){
+                axios.get('https://flynn.boolean.careers/exercises/api/random/mail')
+                .then (function(result){
+                    self.emails[i]=result.response.response;
+                    console.log(i);
+                });
+            }
+        }
     }
 });
